18c3434660bc824ffd9c8d9c6f4b77ab3b9820d4
Gabi2511 <143661645+Gabi2511@users.noreply.github.com>
2025-09-26 08:47:32 -0300

    salvar

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const
+        const proximoPasso
      })
 })
